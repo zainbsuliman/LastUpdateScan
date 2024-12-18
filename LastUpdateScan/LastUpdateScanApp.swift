3b21fc4e2e27bf02fbacd23f3333df1d8525080e
zainbsuliman <zat301099@gmail.com>
2024-12-18 11:33:37 +0300

    Initial Commit

---
+++ LastUpdateScan/LastUpdateScanApp.swift
@@ -0,0 +1,17 @@
+//
+//  LastUpdateScanApp.swift
+//  LastUpdateScan
+//
+//  Created by Zainab Alatwi  on 17/06/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct LastUpdateScanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
